4bd58b527a48ce9d9729daa2803121950d67c2f3
Jerald Tapalla <jeraldtapz@gmail.com>
2019-09-13 22:03:16 +0800

    IO
    Created basic system to read and write from disk
    Implemented save handler and load handler for both binary and json serialization

---
+++ Core.Common/IO/Implementations/BaseSaveHandler.cs
@@ -0,0 +1,67 @@
+﻿using Core.Common;
+using System;
+using System.IO;
+
+namespace Core.SaveSystem
+{
+    public abstract class BaseSaveHandler : ISaveHandler
+    {
+        #region Protected Fields
+
+        protected string saveDirectory;
+        protected readonly Subject<string> logObservable;
+
+        #endregion Protected Fields
+
+        #region ILogger Properties
+
+        public IObservable<string> LogObservable => logObservable;
+
+        #endregion ILogger Properties
+
+        #region Constructors
+
+        protected BaseSaveHandler()
+        {
+            saveDirectory = string.Empty;
+            logObservable = new Subject<string>();
+        }
+
+        #endregion Constructors
+
+        #region ISaveHandler Methods
+
+        public bool SetSaveDirectory(string directory)
+        {
+            saveDirectory = directory;
+
+            if (Directory.Exists(directory))
+                return true;
+            try
+            {
+                Directory.CreateDirectory(directory);
+            }
+            catch (Exception e)
+            {
+                Log($"An error has occured while setting the save directory: {e.Message}");
+                return false;
+            }
+
+            Log("Save Directory successfully configured!");
+            return true;
+        }
+
+        public abstract bool Save(SaveData obj, string overrideName = "savefile", string extension = "bin");
+
+        #endregion ISaveHandler Methods
+
+        #region Private Methods
+
+        protected void Log(string log)
+        {
+            logObservable.OnNext(log);
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ Core.Common/IO/Interfaces/ISaveHandler.cs
@@ -0,0 +1,14 @@
+﻿using Core.Common;
+
+namespace Core.SaveSystem
+{
+    /// <summary>
+    /// Handles saving of data
+    /// </summary>
+    public interface ISaveHandler : ILogger
+    {
+        bool SetSaveDirectory(string directory);
+
+        bool Save(SaveData obj, string overrideName = "savefile", string extension = "bin");
+    }
+}
